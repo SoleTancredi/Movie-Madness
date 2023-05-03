83122f8944b78f1860d08341003fe5f08d1dbfbb
Tancredi M.S <tancredi620@gmail.com>
2023-05-03 18:10:56 -0300

    mapper

---
+++ movies-madness-back/src/main/java/com/soletancredi/moviesmadnessback/mapper/MovieMapper.java
@@ -0,0 +1,37 @@
+package com.soletancredi.moviesmadnessback.mapper;
+
+import com.soletancredi.moviesmadnessback.persistence.model.Movie;
+import com.soletancredi.moviesmadnessback.rest.dto.CreateMovieRequest;
+import com.soletancredi.moviesmadnessback.rest.dto.MovieDto;
+import com.soletancredi.moviesmadnessback.rest.dto.UpdateMovieRequest;
+import com.soletancredi.moviesmadnessback.service.UserService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        uses = UserService.class
+)
+public abstract class MovieMapper {
+    @Autowired
+    protected UserService userService;
+
+    @Mapping(target = "comments", ignore = true)
+    public abstract Movie toMovie(CreateMovieRequest createMovieRequest);
+
+    @Mapping(target = "imdbId", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    public abstract void updateMovieFromDto(UpdateMovieRequest updateMovieRequest, @MappingTarget Movie movie);
+
+    public abstract MovieDto toMovieDto(Movie movie);
+
+    @Mapping(
+            target = "avatar",
+            expression = "java( userService.getUser(comment.getUsername()).isPresent() ? userService.getUser(comment.getUsername()).get().getAvatar() : comment.getUsername() )"
+    )
+    public abstract MovieDto.CommentDto toMovieDtoCommentDto(Movie.Comment comment);
+}
