ce2917dbd8ff29617b50c4934743542e23c0d538
Tancredi M.S <tancredi620@gmail.com>
2023-05-04 15:53:14 -0300

    add security

---
+++ movies-madness-back/src/main/java/com/soletancredi/moviesmadnessback/security/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.soletancredi.moviesmadnessback.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+/*
+ provides a corsFilter bean that can be used in a Spring application
+ to enable CORS filtering. Is used to allow HTTP requests to be made
+ from cross sources.
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(allowedOrigins);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
